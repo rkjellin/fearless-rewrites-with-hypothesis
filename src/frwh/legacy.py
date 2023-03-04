48d9c0007b6721230343f4be4b22f4552a9a2413
Rickard <rickard@normative.io>
2023-03-04 20:46:05 +0100

    handle left extrapol

--- src/frwh/legacy.py
+++ src/frwh/legacy.py
@@ -5,7 +5,6 @@ import pandas as pd
 
 def calculate_relative_change(d1: date, d2: date, obs: dict[date, float]) -> float:
     s = pd.Series(obs)
-    s[d1] = pd.NaT
-    s[d2] = pd.NaT
-    s = s.sort_index().ffill().bfill()
+    s2 = pd.Series({d1: pd.NaT, d2: pd.NaT})
+    s = s.combine_first(s2).sort_index().ffill().bfill()
     return s[d2] / s[d1]
